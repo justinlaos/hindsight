2520abd2bd447b9fa68c2d9fd296e5ff8373d358
Justin Laos <justinlaos@Justins-MacBook-Pro.local>
2022-06-09 18:43:44 -0700

    updates for render deployment

--- config/config.exs
+++ config/config.exs
@@ -13,6 +13,7 @@ config :histora,
 # Configures the endpoint
 config :histora, HistoraWeb.Endpoint,
   url: [host: "localhost"],
+  secret_key_base: "RhlmeZgtzsUAdT96KhAJU629D4W46Z37bYDyMBvIJpJlRvnhMRGScH5fN9ksKLel"
   render_errors: [view: HistoraWeb.ErrorView, accepts: ~w(html json), layout: false],
   pubsub_server: Histora.PubSub,
   live_view: [signing_salt: "T7ClLDU5"]
@@ -24,7 +25,7 @@ config :histora, HistoraWeb.Endpoint,
 #
 # For production it's recommended to configure a different adapter
 # at the `config/runtime.exs`.
-config :histora, Histora.Mailer, adapter: Swoosh.Adapters.Local
+# config :histora, Histora.Mailer, adapter: Swoosh.Adapters.Local
 
 # Swoosh API client is needed for adapters other than SMTP.
 config :swoosh, :api_client, false
