d5bafbe74c0fa4c571a3edaccd6ffd9d2f5d77e3
Justin Laos <justinlaos@Justins-MacBook-Pro.local>
2022-06-09 21:02:44 -0700

    updates for render

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ mix deps.get --only prod
 MIX_ENV=prod mix compile
 
 # Compile assets
-npm install --prefix ./assets
-npm run deploy --prefix ./assets
-mix phx.digest
+# npm install --prefix ./assets
+# npm run deploy --prefix ./assets
+# mix phx.digest
 
 # Build the release and overwrite the existing release directory
 MIX_ENV=prod mix release --overwrite
