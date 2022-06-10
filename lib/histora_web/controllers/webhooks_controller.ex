33f328d73cb2a0a97dc84a151cd26e652829bc73
Justin Laos <justinlaos@Justins-MacBook-Pro.local>
2022-06-10 16:00:41 -0700

    updates for stripe

--- lib/histora_web/controllers/webhooks_controller.ex
+++ lib/histora_web/controllers/webhooks_controller.ex
@@ -28,47 +28,40 @@ defmodule WebhooksController do
     defp handle_webhook(%{"type" => "customer.subscription.created"} = stripe_event) do
         case Stripe.Customer.retrieve(stripe_event["data"]["object"]["customer"]) do
             {:ok, %Stripe.Customer{email: stripe_customer}} -> {
-                if organization = Organizations.get_organization_by_billing_email(stripe_customer) do
+                # organization = Organizations.get_organization_by_billing_email(stripe_customer)
 
-                    Organizations.update_organization(organization, %{
-                        "stripe_price_id" => stripe_event["data"]["object"]["plan"]["id"],
-                        "stripe_product_id" => stripe_event["data"]["object"]["plan"]["product"],
-                        "stripe_customer_id" => stripe_event["data"]["object"]["customer"],
-                        "stripe_subscription_id" => stripe_event["data"]["object"]["id"],
-                        "user_limit" => String.to_integer(stripe_event["data"]["object"]["plan"]["metadata"]["user_limit"]),
-                        "status" => "active",
-                    })
+                # Organizations.update_organization(organization, %{
+                #     "stripe_price_id" => stripe_event["data"]["object"]["plan"]["id"],
+                #     "stripe_product_id" => stripe_event["data"]["object"]["plan"]["product"],
+                #     "stripe_customer_id" => stripe_event["data"]["object"]["customer"],
+                #     "stripe_subscription_id" => stripe_event["data"]["object"]["id"],
+                #     "user_limit" => String.to_integer(stripe_event["data"]["object"]["plan"]["metadata"]["user_limit"]),
+                #     "status" => "active",
+                # })
 
-                else
-
-                    case Organizations.create_organization(%{
-                        "stripe_price_id" => stripe_event["data"]["object"]["plan"]["id"],
-                        "stripe_product_id" => stripe_event["data"]["object"]["plan"]["product"],
-                        "stripe_customer_id" => stripe_event["data"]["object"]["customer"],
-                        "stripe_subscription_id" => stripe_event["data"]["object"]["id"],
-                        "billing_email" => stripe_customer,
-                        "user_limit" => String.to_integer(stripe_event["data"]["object"]["plan"]["metadata"]["user_limit"]),
-                        "name" => "New Organization",
-                        "status" => "active",
-                    }) do
-                        {:ok, organization} ->
-                            password = UUID.uuid4(:hex)
-                            case Users.create_admin(%{
-                                "email" => stripe_customer,
-                                "password" => password,
-                                "password_confirmation" => password,
-                                "role" => "admin",
-                                "organization_id" => organization.id
-                            }) do
-                                {:ok, user} ->
-                                    IO.inspect(user)
-                                {:error, error} ->
-                                    IO.puts(error)
-                            end
-                        {:error, error} ->
-                            IO.puts(error)
-                    end
-
+                case Organizations.create_organization(%{
+                    "stripe_price_id" => stripe_event["data"]["object"]["plan"]["id"],
+                    "stripe_product_id" => stripe_event["data"]["object"]["plan"]["product"],
+                    "stripe_customer_id" => stripe_event["data"]["object"]["customer"],
+                    "stripe_subscription_id" => stripe_event["data"]["object"]["id"],
+                    "billing_email" => stripe_customer,
+                    "user_limit" => String.to_integer(stripe_event["data"]["object"]["plan"]["metadata"]["user_limit"]),
+                    "name" => "New Organization",
+                    "status" => "active",
+                }) do
+                    {:ok, organization} ->
+                        password = UUID.uuid4(:hex)
+                        case Users.create_admin(%{
+                            "email" => stripe_customer,
+                            "password" => password,
+                            "password_confirmation" => password,
+                            "role" => "admin",
+                            "organization_id" => organization.id
+                        }) do
+                            {:ok, user} -> IO.inspect(user)
+                            {:error, error} -> IO.puts(error)
+                        end
+                    {:error, error} -> IO.puts(error)
                 end
             }
             {:error, error} -> IO.inspect(error)
