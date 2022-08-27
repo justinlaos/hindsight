16370d42e5e8cda9c0122e2962b6e9d81eeefda3
Justin Laos <justinlaos@Justins-MacBook-Pro.local>
2022-08-26 22:16:10 -0700

    fix for sendgrid sender and site seo

--- lib/histora/email.ex
+++ lib/histora/email.ex
@@ -26,7 +26,7 @@ defmodule Histora.Email do
 
     defp base_email do
         new_email()
-        |> from("4jlaos@gmail.com") # Set a default from
+        |> from("team@histora.app") # Set a default from
         # |> put_html_layout({MyApp.LayoutView, "email.html"}) # Set default layout
         # |> put_text_layout({MyApp.LayoutView, "email.text"}) # Set default text layout
     end
